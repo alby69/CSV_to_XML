498392a046e526a699ff6d13d77f109ecf884787
wtarr <william.projects.2012@gmail.com>
2013-08-08 19:29:20 +0100

    Load and learn root, element and attribute tags from first two rows

--- CSV_to_XML.py
+++ CSV_to_XML.py
@@ -1,1 +1,52 @@
 __author__ = 'William'
+import csv
+import sys
+
+xmloutput = ""
+root = ""
+element = ""
+attributes = []
+opentag = '<'
+closetag = '>'
+
+
+def loadFile(filename):
+    file = open(filename)
+    try:
+        reader = csv.reader(file)
+        extractHeadings(reader)
+    finally:
+        file.close()
+
+
+def extractHeadings(reader):
+    global root, element, attributes
+    select = reader.next()
+    root = select[0]
+    element = select[1]
+    select = reader.next()
+    attributes = select
+    buildxml(reader)
+
+
+def buildxml(reader):
+    global xmloutput
+
+    xmloutput += '\n' + opentag + root + closetag  #<root>
+    xmloutput += '\n' + '\t' + opentag + element + closetag  #<element>
+    #<attributes>
+    xmloutput += '\n' + '\t' + opentag + '/' + element + closetag  #</element>
+    xmloutput += '\n' + opentag + '/' + root + closetag  #</root>
+
+
+
+
+
+
+    for row in reader:
+        print row
+
+
+if __name__ == '__main__':
+    loadFile(str(sys.argv[1]))
+    print xmloutput
